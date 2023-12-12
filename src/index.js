0b35b3de65250ff2c78964f362ea7fd464415ef8
Vuzuize <84097876+vuvip113@users.noreply.github.com>
2023-12-12 15:56:42 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,31 @@
 const express = require("express");
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 const mongoose = require("mongoose");
-const routes = require("./routes");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-dotenv.config();
+const routes = require('./routes')
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
-const app = express();
-const port = process.env.PORT || 3001;
+dotenv.config()
 
-app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(
-  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
-);
-app.use(bodyParser.json());
-app.use(cookieParser());
+const app = express()
+const port = process.env.PORT || 3001
+
+app.use(cors())
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+app.use(bodyParser.json())
+app.use(cookieParser())
 
 routes(app);
 
-mongoose
-  .connect(`${process.env.MONGO_DB}`)
-  .then(() => {
-    console.log("Ket Noi MONGO_DB thanh cong");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-console.log("id", process.env.CLIENT_ID);
-
+mongoose.connect(`${process.env.MONGO_DB}`)
+    .then(() => {
+        // console.log('Connect Db success!')
+    })
+    .catch((err) => {
+        // console.log(err)
+    })
 app.listen(port, () => {
-  console.log("Server dang chay :" + port);
-});
+    // console.log('Server is running in port: ', + port)
+})
